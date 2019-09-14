ddc902786a9cc0671d46174f9fbdf49247f484e3
cvaughan1640@hotmail.com <cvaughan1640@hotmail.com>
2019-09-14 01:18:50 +0100

    Adding sudo to YUM command

--- scripts/centos-database.sh
+++ scripts/centos-database.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 sudo rpm -Uvh http://dev.mysql.com/get/mysql-community-release-el7-5.noarch.rpm
-yum install -y mysql mysql-server mysql-devel
+sudo yum install -y mysql mysql-server mysql-devel
 sudo systemctl start mysqld
 sudo service mysqld start
 
